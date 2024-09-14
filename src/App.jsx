5dd27c79d11e04194ed45d88af01c5cf1c8210c3
Srihari R <120409108+srihari-976@users.noreply.github.com>
2024-09-14 09:57:53 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import Encryption from './components/encryption';
 import Decryption from './components/decryption';
+import CreateChatRoom from './components/CreateChatRoom'; 
+import ChatRoom from './components/ChatRoom';
+import ChatEntry from './components/ChatEntry';
+import EnterRoom from './components/EnterRoom';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/encryption" element={<Encryption />} />
           <Route path="/decryption" element={<Decryption />} />
+          <Route path="/create-chat-room" element={<CreateChatRoom />} />
+          <Route path="/" element={<ChatEntry />} />
+          <Route path="/enter-room" component={EnterRoom} />
+          <Route path="/chat-room/:roomName" element={<ChatRoom />} />
         </Routes>
       </div>
     </Router>
